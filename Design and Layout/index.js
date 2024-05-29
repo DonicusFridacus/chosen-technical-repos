1726ff302f5f452eed6cc9c62f2d402c4a83f46a
Rujeth Lianne Tanchico <jethhtejlianne@gmail.com>
2024-05-30 01:36:37 +0400

    Update
    - index.js = comments added

--- Design and Layout/index.js
+++ Design and Layout/index.js
@@ -1,35 +1,46 @@
+// --- Our Services (Home Page) ---
 var servicefour = document.getElementById("srvce-4")
 var servicefive = document.getElementById("srvce-5")
 var servicesix = document.getElementById("srvce-6")
 var serviceseven = document.getElementById("srvce-7")
 var serviceeight = document.getElementById("srvce-8")
 
-var viewMoreBtn = document.getElementById("view-more")
+// When View More is clicked the whole list of services will be shown. 
+var viewMoreBtn = document.getElementById("view-more") 
 
+// Media Queries
 var standardPhone = window.matchMedia("(max-width: 700px)")
 var phone_tablet = window.matchMedia("(min-width: 701px) and (max-width: 1023px)")
 var tablet_laptop = window.matchMedia("(min-width: 1024px)")
 
 function viewMoreServices(){
+    // On screen widths of 700px and under. 
     if (standardPhone.matches) {
         servicefour.style.display = "block"
         servicefive.style.display = "block"
         servicesix.style.display = "block"
         serviceseven.style.display = "block"
         serviceeight.style.display = "block"
-    } else if (phone_tablet.matches){
+    } 
+    // On screen widths of 701px to 1023px.
+    else if (phone_tablet.matches){
         servicefive.style.display = "block"
         servicesix.style.display = "block"
         serviceseven.style.display = "block"
         serviceeight.style.display = "block"
-    } else{
+    } 
+    // On screen widths of 1024px and above.
+    else{
         serviceseven.style.display = "block"
         serviceeight.style.display = "block"
     }
+    // Once the full list of services are shown, the option to "View More" disappears.
     viewMoreBtn.style.display = "none"
 }
 
-//Review Slider Functionality
+
+//--- Review Slider Functionality ---
+
 // Reference: https://www.codingnepalweb.com/draggable-card-slider-html-css-javascript/
 const reviewSection = document.querySelector(".review-section");
 const reviewCarousel = document.querySelector(".review-carousel");
@@ -60,7 +71,9 @@ reviewCarousel.classList.remove("no-transition");
 // Event Listeners are added to allow scrolling to left and right using the buttons.
 btnsPrevNext.forEach(buttons => {
     buttons.addEventListener("click", () => {
+        // Scrolls to the left when Previous button is clicked.
         reviewCarousel.scrollLeft += buttons.className == "prev-btn" ? -firstReviewWidth : firstReviewWidth;
+        // Scrolls to the right when Next button is clicked.
         reviewCarousel.scrollRight += buttons.className == "next-btn" ? +firstReviewWidth : firstReviewWidth;
     });
 });
@@ -81,13 +94,12 @@ const dragStop = () => {
     reviewCarousel.classList.remove("dragging");
 }
 const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
+    // When the first few review cards are shown, carousel will scroll to the end. and vice versa.
     if(reviewCarousel.scrollLeft === 0) {
         reviewCarousel.classList.add("no-transition");
         reviewCarousel.scrollLeft = reviewCarousel.scrollWidth - (2 * reviewCarousel.offsetWidth);
         reviewCarousel.classList.remove("no-transition");
     }
-    // If the carousel is at the end, scroll to the beginning
     else if(Math.ceil(reviewCarousel.scrollLeft) === reviewCarousel.scrollWidth - reviewCarousel.offsetWidth) {
         reviewCarousel.classList.add("no-transition");
         reviewCarousel.scrollLeft = reviewCarousel.offsetWidth;
